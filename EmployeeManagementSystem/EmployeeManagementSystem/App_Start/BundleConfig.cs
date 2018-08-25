c9c2e9a751849b8e4327e5a41d70af4d2a40e5d7
SharedOps <manikanta.mundru@gmail.com>
2018-08-26 01:17:06 +0530

    employee management system

---
+++ EmployeeManagementSystem/EmployeeManagementSystem/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace EmployeeManagementSystem
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/js").Include(
+                "~/js/angular.js",
+                "~/js/app.js"));
+            
+            
+            bundles.Add(new StyleBundle("~/css").Include(
+                "~/css/bootstrap.css"));
+
+            
+        }
+    }
+}
---
+++ MvcMusicStore/MvcMusicStore/Models/MusicStoreDbInitializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+    public class MusicStoreDbInitializer : System.Data.Entity.DropCreateDatabaseAlways<MusicStoreDb>
+    {
+        protected override void Seed(MusicStoreDb context)
+        {
+            context.Artists.Add(new Artist { Name = "Okka Magadu" });
+            context.Genres.Add(new Genre { Name = "Rock" });
+            context.Albums.Add(new Album
+            {
+                Artist = new Artist { Name = "DSP" },
+                Genre = new Genre { Name = "Rock" },
+                Price = 9.99m,
+                Title = "Caravan"
+            });
+            base.Seed(context);
+        }
+    }
+
+}
